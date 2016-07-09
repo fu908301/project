8685c4f134e3ab6afb5378fe53d0d279ceb79344
CHENG-AN FU <fu908301@gmail.com>
2016-07-10 00:48:33 +0800

    Key的傳遞問題目前無解(目前是因為系統READ-ONLY導致無法傳遞)

--- UME_project/app/src/main/java/com/example/andy/ume_project/MainActivity.java
+++ UME_project/app/src/main/java/com/example/andy/ume_project/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.andy.ume_project;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.io.*;
 public class MainActivity extends AppCompatActivity {
-
+    Button button1;
+    EditText key;
+    int times = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.button);
+        key = (EditText) findViewById(R.id.editText);
+        button1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                times = times + 1;
+                try {
+                    toNext();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Your key is : ");
+                builder.setMessage(key.getText());
+                builder.show();
+            }
+        });
+    }
+    public void toNext() throws IOException {
+        String stringtimes = Integer.toString(times);
+        FileWriter fw1 = new FileWriter("key");
+        FileWriter fw2 = new FileWriter("time");
+        fw1.write(key.getText().toString());
+        fw2.write(stringtimes);
+        fw1.close();
+        fw2.close();
     }
 }
+
+
--- UME_project/app/src/main/java/com/example/andy/ume_project/SimpleIME.java
+++ UME_project/app/src/main/java/com/example/andy/ume_project/SimpleIME.java
@@ -1,24 +1,53 @@
 package com.example.andy.ume_project;
+import android.app.Activity;
+import android.content.Intent;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.media.AudioManager;
+import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
+import java.io.*;
 
 /**
  * Created by Andy on 2016/5/17.
  */
-public class SimpleIME extends InputMethodService
-        implements KeyboardView.OnKeyboardActionListener {
-    String key = "abcde";
+
+public class SimpleIME extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    int times = 0;
+    int temptimes;
     int index = 0;
     private KeyboardView kv;
     private Keyboard keyboard;
     private boolean caps = false;
     private boolean encrypt = false;
-
+    String tempkey;
+    String mykey = "aaa";
+    public void receive() throws IOException {
+        char buffer1[] = new char[1024];
+        char buffer2[] = new char[1024];
+        FileReader fr1;
+        FileReader fr2;
+        try {
+            fr1 = new FileReader("time");
+            int len1 = fr1.read(buffer1);
+            String stime = new String(buffer1,0,len1);
+            temptimes = Integer.parseInt(stime);
+            fr1.close();
+        }catch (FileNotFoundException ex){
+            ex.printStackTrace();
+        }
+        try {
+            fr2 = new FileReader("key");
+            int len2 = fr2.read(buffer2);
+            tempkey = new String(buffer2,0,len2);
+            fr2.close();
+        }catch (FileNotFoundException ex){
+            ex.printStackTrace();
+        }
+    }
     @Override
     public void onKey(int primaryCode, int[] keyCodes) {
         InputConnection ic = getCurrentInputConnection();
@@ -44,6 +73,16 @@ public class SimpleIME extends InputMethodService
                     code = Character.toUpperCase(code);
                 }
                 if(encrypt){
+                    try {
+                        receive();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if(temptimes == times + 1){
+                        times = temptimes;
+                        mykey = tempkey;
+                        index = 0;
+                    }
                     int test;
                     test = (int)code;
                     code = myencrypt(test);
@@ -54,10 +93,10 @@ public class SimpleIME extends InputMethodService
 
     public char myencrypt(int input) {
         char returnchar;
-        int mykey =(int)key.charAt(index);
-        input = (input + mykey) % 128;
+        int intkey =(int)mykey.charAt(index);
+        input = (input +intkey) % 128;
         index++;
-        if(index == key.length())
+        if(index == mykey.length())
             index = 0;
         returnchar = (char)input;
         return returnchar;
